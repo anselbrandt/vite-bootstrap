daeab4b6ca3d8e8cc25bb99c36ed1c1ebd891caf
ansel brandt <ansel@anselbrandt.com>
2023-04-10 16:28:50 -0400

    Add fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="d-flex justify-content-center">Hello</div>;
+  return (
+    <div className="d-flex justify-content-center">
+      <div>Hello</div>
+      <div>
+        <i className="far fa-spinner fa-spin mr-2"></i>
+      </div>
+    </div>
+  );
 }
 
 export default App;
