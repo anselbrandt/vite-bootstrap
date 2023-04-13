caa3c25a4d5e46442c9435b1459320f8842c7520
ansel brandt <ansel@anselbrandt.com>
2023-04-13 16:55:52 -0400

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { Card } from "./ui/Card";
+import { Alert } from "./ui";
+
 function App() {
   return (
-    // <div className="d-flex justify-content-center">
-    <div>
-      <Card>
-        <Card.Header>Card Header</Card.Header>
-        <Card.Title> Card Title</Card.Title>
-        <Card.Body>Card Body</Card.Body>
-      </Card>
+    <div className="d-flex justify-content-center">
+      <div className="alert alert-secondary" role="alert">
+        <a href={"/"} className="alert-link">
+          <i className="fas fa-calendar-plus mr-1" />
+          Hello
+        </a>
+      </div>
+      <Alert variant="secondary" icon="calendar-plus">
+        <Alert.Link to="/">Hello</Alert.Link>
+      </Alert>
     </div>
   );
 }
